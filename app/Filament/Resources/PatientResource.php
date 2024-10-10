0444b2cc1e0f763f0c9751a62486859ea40e513a
Naveed Amin <naveedmughal1182@gmail.com>
2024-10-10 11:55:10 +0500

    done

--- app/Filament/Resources/PatientResource.php
+++ app/Filament/Resources/PatientResource.php
@@ -7,11 +7,16 @@ use App\Filament\Resources\PatientResource\RelationManagers;
 use App\Filament\Resources\PatientResource\RelationManagers\TreatmentsRelationManager;
 use App\Models\Patient;
 use Filament\Forms;
+use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
+use Filament\Support\RawJs;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
@@ -29,7 +34,78 @@ class PatientResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('name')->required()->maxLength(255),
+                TextInput::make('name')->required()->maxLength(255)->autocapitalize('words'),
+                TextInput::make('backgroundColor')
+                    ->type('color'),
+                TextInput::make('number')
+                    ->numeric()
+                    ->step(100),
+                    TextInput::make('cardNumber')
+    ->mask(RawJs::make(<<<'JS'
+        $input.startsWith('34') || $input.startsWith('37') ? '9999 999999 99999' : '9999 9999 9999 9999'
+    JS)),
+    TextInput::make('cost')
+    ->prefix('€')
+    ->suffixAction(
+        Action::make('copyCostToPrice')
+            ->icon('heroicon-m-clipboard')
+            ->requiresConfirmation()
+            ->action(function (Set $set, $state) {
+                $set('price', $state);
+            })
+        ),
+    Tabs::make('Tabs')
+    ->tabs([
+        Tabs\Tab::make('Tab 1')
+            ->schema([
+                TextInput::make('name')->required()->maxLength(255)->autocapitalize('words'),
+            ]),
+        Tabs\Tab::make('Tab 2')
+            ->schema([
+                TextInput::make('father_name')->required()->maxLength(255)->autocapitalize('words'),
+            ]),
+        Tabs\Tab::make('Tab 3')
+            ->schema([
+                TextInput::make('phone')->required()->maxLength(255)->autocapitalize('words'),
+            ]),
+        ]),
+        Wizard::make([
+            Wizard\Step::make('Order')
+                ->schema([
+                    // ...
+                ]),
+            Wizard\Step::make('Delivery')
+                ->schema([
+                    // ...
+                ]),
+            Wizard\Step::make('Billing')
+                ->schema([
+                    // ...
+                ]),
+            ]),
+                TextInput::make('password')
+                    ->password()
+                    ->autocomplete('new-password')->revealable(),
+                TextInput::make('birthday')
+                    ->mask('99/99/9999')
+                    ->placeholder('MM/DD/YYYY'),
+                TextInput::make('manufacturer')
+                    ->datalist([
+                        'BWM',
+                        'Ford',
+                        'Mercedes-Benz',
+                        'Porsche',
+                        'Toyota',
+                        'Tesla',
+                        'Volkswagen',
+                    ]),
+                TextInput::make('domain')
+                    ->url()
+                    ->suffixIcon('heroicon-m-check-circle')
+                    ->suffixIconColor('success'),
+                TextInput::make('domain')
+                    ->prefix('https://')
+                    ->suffix('.com'),
                 Select::make('type')->options([
                     'cat' => 'Cat',
                     'dog' => 'Dog',
@@ -39,21 +115,21 @@ class PatientResource extends Resource
                     'bird' => 'Bird',
                 ])->required(),
                 DatePicker::make('date_of_birth')->required()->maxDate(now()),
-                Select::make('owner_id')->required()->relationship('owner','name')->searchable()->preload()
-                ->createOptionForm([
-                    Forms\Components\TextInput::make('name')
-                        ->required()
-                        ->maxLength(255),
-                    Forms\Components\TextInput::make('email')
-                        ->label('Email address')
-                        ->email()
-                        ->required()
-                        ->maxLength(255),
-                    Forms\Components\TextInput::make('phone')
-                        ->label('Phone number')
-                        ->tel()
-                        ->required(),
-                ]),
+                Select::make('owner_id')->required()->relationship('owner', 'name')->searchable()->preload()
+                    ->createOptionForm([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('email')
+                            ->label('Email address')
+                            ->email()
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('phone')
+                            ->label('Phone number')
+                            ->tel()
+                            ->required(),
+                    ]),
             ]);
     }
 
@@ -68,14 +144,14 @@ class PatientResource extends Resource
             ])
             ->filters([
                 SelectFilter::make('type')
-                ->options([
-                    'cat' => 'Cat',
-                    'dog' => 'Dog',
-                    'rabbit' => 'Rabbit',
-                    'parrot' => 'Parrot',
-                    'hamster' => 'Hamster',
-                    'bird' => 'Bird',
-                ])
+                    ->options([
+                        'cat' => 'Cat',
+                        'dog' => 'Dog',
+                        'rabbit' => 'Rabbit',
+                        'parrot' => 'Parrot',
+                        'hamster' => 'Hamster',
+                        'bird' => 'Bird',
+                    ])
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
